a4ff27e29d4c6e4d7d307a40d3fefd64a8cb4b53
KevlarWai <kevinwai@hotmail.co.uk>
2021-06-01 20:19:44 +0100

    Use useState to retrieve input
    User userState to retrieve entered values upon submit button

--- src/Components/AddUsers/AddUserForm.js
+++ src/Components/AddUsers/AddUserForm.js
@@ -1,18 +1,50 @@
 import "./AddUserForm.css";
+import React, { useState } from "react";
 
 const AddUserForm = () => {
+  const [enteredUsername, setUsername] = useState("");
+  const [enteredAge, setAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setAge(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("username:");
+    console.log(enteredUsername);
+    console.log("age:");
+    console.log(enteredAge);
+    setUsername("");
+    setAge("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="add-user__controls">
         <div>
           <label>Username</label>
-          <input></input>
+          <input
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHandler}
+          ></input>
         </div>
         <div>
           <label>Age (Years)</label>
-          <input></input>
+          <input
+            type="number"
+            min="0"
+            step="1"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          ></input>
         </div>
-        <div className='.add-user__actions'>
+        <div className=".add-user__actions">
           <button type="submit">Add User</button>
         </div>
       </div>
