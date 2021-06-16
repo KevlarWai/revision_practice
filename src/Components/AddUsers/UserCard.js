e13f7c7171121e9a7be6f91862c031a6ad575ab6
KevlarWai <kevinwai@hotmail.co.uk>
2021-06-16 18:17:10 +0100

    Register click on list item
    Register click on a list item

--- src/Components/AddUsers/UserCard.js
+++ src/Components/AddUsers/UserCard.js
@@ -1,18 +1,26 @@
 import Card from "../UI/Card";
 import classes from "./UserCard.module.css";
+import React, { useState } from "react";
 
 const UserCard = (props) => {
+  const [card, setCard] = useState(props.userCard);
+
   console.log(props.userCard);
 
   if (props.userCard.length === 0) {
     return null;
   }
 
+  const handleClick = (event) => {
+    console.log("clicked!");
+    console.log(event.target.value);
+  };
+
   return (
     <Card className={classes.users}>
       <ul>
         {props.userCard.map((user) => (
-          <li key={user.id}>
+          <li value={user.id} key={user.id} onClick={handleClick}>
             {user.username} ({user.age} Years Old)
           </li>
         ))}
